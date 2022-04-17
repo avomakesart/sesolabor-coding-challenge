35d922006d971895e7727e1c37206470ae460ed2
Alvaro Castillo <alvcastillo@paypal.com>
2022-04-17 03:55:01 -0500

    feat: challenge completed

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,51 @@
-"use strict";
+'use strict';
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
+/*
+ *
+ * Async Solution:
+ *
+ * Ok so in the sync solution I used the Array prototype to
+ * complete the task, but in this case, since it's an async operation
+ * I consider that using that Array stuff doesn't matter, so I used
+ * a classic for loop because of readability purposes.
+ *
+ */
+
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    const logsLength = logSources.length;
+    const logStack = [];
+    const limit = 10000;
+
+    if (logsLength > limit) {
+      for (let index = 0; index < logsLength; index++) {
+        const element = logSources[index];
+        logEntriesPromise.push(element.popAsync());
+      }
+    } else {
+      for (const source of logSources) {
+        logStack.push(source.popAsync());
+      }
+    }
+
+    Promise.all(logStack).then((sources) => {
+      sources.sort((a, b) => a.date - b.date);
+
+      if (logsLength > limit) {
+        for (let index = 0; index < logsLength; index++) {
+          const element = sources[index];
+          printer.print(element);
+        }
+      } else {
+        for (const source of sources) {
+          printer.print(source);
+        }
+      }
+      printer.done();
+    });
+
+    resolve(console.log('Async sort complete.'));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,48 @@
-"use strict";
+'use strict';
 
 // Print all entries, across all of the sources, in chronological order.
 
+/*
+ *
+ * Sync Solution:
+ *
+ * in the beginning, I was thinking about using the faster
+ * data structure for sorting a "QuickSort" algorithm,
+ * but I think that it was too much for this, so I decided to use
+ * the Array prototype instead of those boring for and while loops,
+ * and I consider that it is much faster or maybe more fancy haha :P.
+ *
+ */
+
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const logsLength = logSources.length;
+  const logStack = [];
+  const limit = 10000;
+
+  if (logsLength > limit) {
+    Array.from(({ logStack }) => (_, index) => {
+      logStack.push(logSources[index].pop());
+      index++;
+    });
+  } else {
+    for (const source of logSources) {
+      logStack.push(source.pop());
+    }
+  }
+
+  logStack.sort((a, b) => a.date - b.date);
+
+  if (logsLength > limit) {
+    Array.from(({ logStack }) => (_, index) => {
+      printer.print(logStack[index]);
+      index++;
+    });
+  } else {
+    for (const source of logStack) {
+      printer.print(source);
+    }
+  }
+
+  printer.done();
+  return console.log('Sync sort complete.');
 };
