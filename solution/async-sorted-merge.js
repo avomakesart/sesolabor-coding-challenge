76eb4e89594fb22838ad84ea91d2e06ceda2b49d
Alvaro Castillo <alvcastillo@paypal.com>
2022-04-18 21:25:30 -0500

    fix: adding another approach to make it better and more mantainable

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -6,46 +6,71 @@
  *
  * Async Solution:
  *
- * Ok so in the sync solution I used the Array prototype to
- * complete the task, but in this case, since it's an async operation
- * I consider that using that Array stuff doesn't matter, so I used
- * a classic for loop because of readability purposes.
+ * I was thinking using async await with a trycatch instead of a big
+ * Promise, but for the seak of this challenge I used Promises.
  *
+ * and... well I ended up using a while :laugh: since it is faster in this case.
+ *
+ * This solution it's similar to sync one,  We sort the logs, and then
+ * we find the insert position of new element in sorted array, using the findIndex function below.
+ *
+ * Then we splice the array that replaces printed log entries
+ * with the next entry popped from the same source.
+ *
+ * But we use Promises and at the end we resolve the final console.log
  */
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    const logsLength = logSources.length;
-    const logStack = [];
-    const limit = 10000;
+  return new Promise(async (resolve, reject) => {
+    const logItems = await Promise.all(
+      logSources.map(async (logSource, index) => {
+        const logItem = await logSource.popAsync();
+        return {
+          logIndex: index,
+          log: logItem,
+        };
+      })
+    );
 
-    if (logsLength > limit) {
-      for (let index = 0; index < logsLength; index++) {
-        const element = logSources[index];
-        logEntriesPromise.push(element.popAsync());
-      }
-    } else {
-      for (const source of logSources) {
-        logStack.push(source.popAsync());
+    logItems.sort((a, b) => (a.log.date > b.log.date ? 1 : -1));
+
+    while (logItems.length > 0) {
+      const firstLog = logItems.shift();
+      printer.print(firstLog.log);
+
+      const logSource = await logSources[firstLog.logIndex].popAsync();
+
+      // Checking just for safety if
+      // it is different than false
+      if (logSource !== false) {
+        const newElement = {
+          logIndex: firstLog.logIndex,
+          log: logSource,
+        };
+
+        const insertLogPosition = findIndex(logItems, newElement);
+        logItems.splice(insertLogPosition, 0, newElement);
       }
     }
-
-    Promise.all(logStack).then((sources) => {
-      sources.sort((a, b) => a.date - b.date);
-
-      if (logsLength > limit) {
-        for (let index = 0; index < logsLength; index++) {
-          const element = sources[index];
-          printer.print(element);
-        }
-      } else {
-        for (const source of sources) {
-          printer.print(source);
-        }
-      }
-      printer.done();
-    });
-
+    printer.done();
     resolve(console.log('Async sort complete.'));
   });
 };
+
+/*
+ * Since we will be traversing the whole array therefore
+ * the time complexity of algorithm would be O(N).
+ */
+function findIndex(arr, element) {
+  let low = 0,
+    high = arr.length;
+  while (low < high) {
+    let mid = (low + high) >>> 1;
+    if (arr[mid].log.date < element.log.date) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+  return low;
+}
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -8,41 +8,68 @@
  *
  * in the beginning, I was thinking about using the faster
  * data structure for sorting a "QuickSort" algorithm,
- * but I think that it was too much for this, so I decided to use
- * the Array prototype instead of those boring for and while loops,
- * and I consider that it is much faster or maybe more fancy haha :P.
+ *
+ * In this solution, we pop the first log from each log source
+ * to keep track from where it came from.
+ *
+ * We sort the logs, and then we find the insert position of
+ * new element in sorted array, using the findIndex function below.
+ *
+ * Then we splice the array that replaces printed log entries
+ * with the next entry popped from the same source.
  *
  */
 
 module.exports = (logSources, printer) => {
-  const logsLength = logSources.length;
-  const logStack = [];
-  const limit = 10000;
+  const logEntries = logSources.map((source, index) => ({
+    // Trying to see the drained value 🤔
+    drained: source.drained,
+    logIndex: index,
+    log: source.pop(),
+  }));
 
-  if (logsLength > limit) {
-    Array.from(({ logStack }) => (_, index) => {
-      logStack.push(logSources[index].pop());
-      index++;
-    });
-  } else {
-    for (const source of logSources) {
-      logStack.push(source.pop());
-    }
-  }
+  logEntries.sort((a, b) => (a.log.date > b.log.date ? 1 : -1));
+  while (logEntries.length > 0) {
+    // Let's print the first log
+    const firstLog = logEntries.shift();
+    printer.print(firstLog.log);
 
-  logStack.sort((a, b) => a.date - b.date);
+    // new logs
+    const logSource = logSources[firstLog.logIndex].pop();
 
-  if (logsLength > limit) {
-    Array.from(({ logStack }) => (_, index) => {
-      printer.print(logStack[index]);
-      index++;
-    });
-  } else {
-    for (const source of logStack) {
-      printer.print(source);
+    // Checking just for safety if
+    // it is different than false
+    if (logSource !== false) {
+      const newLog = {
+        logIndex: firstLog.logIndex,
+        log: logSource,
+      };
+
+      const insertLogPosition = findIndex(logEntries, newLog);
+      logEntries.splice(insertLogPosition, 0, newLog);
     }
   }
 
   printer.done();
+
   return console.log('Sync sort complete.');
 };
+
+/*
+ * Since we will be traversing the whole array therefore
+ * the time complexity of algorithm would be O(N).
+ */
+
+function findIndex(arr, element) {
+  let low = 0,
+    high = arr.length;
+  while (low < high) {
+    let mid = (low + high) >>> 1;
+    if (arr[mid].log.date < element.log.date) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+  return low;
+}
